bdfbb2c91ce0491ee1d3e8cf23808d9be290ee33
Pratik-ctr <pratikkpatillll26@gmail.com>
2025-06-14 14:27:47 +0530

    Last onemore

--- src/pages/RecentPostPage.jsx
+++ src/pages/RecentPostPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
-import RecentPost from '../components/recentpost/recentpost'
+import RecentPost from '../components/recentpost/Recentpost'
 function RecentPostPage () {
   return (
     <div>
